09a81ea8f701b75a408203dc1083d07a121d3986
ctdupuis <ctdupuis7@gmail.com>
2021-06-08 00:31:23 -0500

    init

---
+++ basics.py
@@ -0,0 +1,19 @@
+name = "Cody"
+age = 10 + 15
+
+print("name: " + name)
+print("age: " + str(age))
+
+## Booleans
+# True and False must be capitalized
+truthy = True
+falsy = False
+
+5 is 5 #=> True
+5 is not 5 #=> False
+5 is not 6 #=> True 
+
+"This" is "This" #=> True
+True is True #=> True
+"True" is True #=> False
+"True" is str(True) #=> True
