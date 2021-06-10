d866ff2db1491f3f39577ea9a9c0d521d53ec880
ctdupuis <ctdupuis7@gmail.com>
2021-06-09 20:55:00 -0500

    calculator finished

--- calculator.py
+++ calculator.py
@@ -1,26 +1,30 @@
 import re
 
 print("Python Calculator")
-print("Type 'quit' to exit.")
+print("Type 'quit' to exit.\n")
 
 previous = 0
 run = True
 
 def perform_math():
     global run
+    global previous
     # equation = ""
     if previous == 0:
         equation = input("Enter equation: ")
     else:
         equation = input(str(previous))
 
-        
+
     if equation == 'quit':
+        print("Goodbye!")
         run = False
     else:
-        equation = re.sub('[a-zA-Z,.;:()+" "]', "", equation)
-        previous = eval(equation)
-        print(previous)
+        equation = re.sub('[a-zA-Z,.;:()" "]', "", equation)
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
 
 while run:
     perform_math()
